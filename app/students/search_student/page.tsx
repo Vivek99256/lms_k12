a721ae434b46c373f06d164d526458ae597fa433
robertparkar99 <rp2164394@gmail.com>
2026-07-03 14:38:51 +0530

    search student

---
+++ app/students/search_student/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { useCallback, useEffect, useState } from 'react';
+
+export default function SearchStudentPage() {
+    return (
+        <div>
+            <h1>Search Student heheh</h1>
+        </div>
+    );
+}
