2bf8994accc5ef00a933f7cbdd25eb737d78521d
harshitpipaliya-phd <pipaliyaharshit2004@gmail.com>
2026-09-23 12:21:54 +0530

    error build solve

---
+++ app/lms/worksheet/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { ExamOperationsScreen } from '@/app/lms/exam/page';
+
+export default function WorksheetPage() {
+  return <ExamOperationsScreen scopedExamType="worksheet" />;
+}
